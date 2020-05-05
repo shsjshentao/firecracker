fc58228da08ba5d4e5ae2307b7b16ff9817b86ef
Andrei Răduță <andrei.raduta11@gmail.com>
2020-05-05 17:15:00 +0300

    PCI: Add support for the Class Code of Function
    - Add structures and traits for the three parts of Class Code.
    - Add the base classes of the PCI, and the subclasses for Bridge.
    
    Signed-off-by: Andrei Răduță <andrei.raduta11@gmail.com>

--- src/pci/src/constants.rs
+++ src/pci/src/constants.rs
@@ -18,3 +18,84 @@ const DEVICE_SPECIFIC_REGISTERS: usize = 48;
 
 /// The PCIe Extended Configuration Registers Space has a length 3840 bytes, so 960 dwords.
 const EXTENDED_CONFIGURATION_REGISTERS: usize = 960;
+
+/// Type 0 is required for every Function, except for the Bridge Functions.
+/// Type 1 is required for the Bridge (Switch) Functions.
+#[derive(Clone, Copy)]
+pub enum PciHeaderType {
+    Type0,
+    Type1,
+}
+
+/// The upper byte of a Class Code.
+/// Classifies the type of functionality that the Function of the Device provides.
+/// https://pcisig.com/sites/default/files/files/PCI_Code-ID_r_1_11__v24_Jan_2019.pdf
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+pub enum PciBaseClass {
+    UnclassifiedDevice = 0x00,
+    MassStorageController = 0x01,
+    NetworkController = 0x02,
+    DisplayController = 0x03,
+    MultimediaController = 0x04,
+    MemoryController = 0x05,
+    BridgeDevice = 0x06,
+    SimpleCommunicationController = 0x07,
+    BaseSystemPeripheral = 0x08,
+    InputDeviceController = 0x09,
+    DockingStation = 0x0A,
+    Processor = 0x0B,
+    SerialBusController = 0x0C,
+    WirelessController = 0x0D,
+    IntelligentIOController = 0x0E,
+    SatelliteCommunicationController = 0x0F,
+    EncryptionController = 0x10,
+    SignalProcessingController = 0x11,
+    ProcessingAccelerator = 0x12,
+    NonEssentialInstrumentation = 0x13,
+    Other = 0xFF,
+}
+
+impl PciBaseClass {
+    pub fn get_register_value(self) -> u8 {
+        self as u8
+    }
+}
+
+/// The middle byte of a Class Code.
+/// Identifies the type of functionality that the the Function of the Device provides.
+/// https://pcisig.com/sites/default/files/files/PCI_Code-ID_r_1_11__v24_Jan_2019.pdf
+pub trait PciSubclass {
+    fn get_register_value(&self) -> u8;
+}
+
+/// Subclasses of the Bridge Device.
+#[allow(dead_code)]
+#[derive(Copy, Clone)]
+pub enum PciBridgeSubclass {
+    HostBridge = 0x00,
+    IsaBridge = 0x01,
+    EisaBridge = 0x02,
+    McaBridge = 0x03,
+    PciToPciBridge = 0x04,
+    PcmciaBridge = 0x05,
+    NuBusBridge = 0x06,
+    CardBusBridge = 0x07,
+    RACEwayBridge = 0x08,
+    PciToPciSemiTransparentBridge = 0x09,
+    InfiniBandToPciHostBridge = 0x0A,
+    OtherBridgeDevice = 0x80,
+}
+
+impl PciSubclass for PciBridgeSubclass {
+    fn get_register_value(&self) -> u8 {
+        *self as u8
+    }
+}
+
+/// The lower byte of a Class Code.
+/// Identifies the specific register-level interface (if any) of a Function.
+/// https://pcisig.com/sites/default/files/files/PCI_Code-ID_r_1_11__v24_Jan_2019.pdf
+pub trait PciProgrammingInterface {
+    fn get_register_value(&self) -> u8;
+}
