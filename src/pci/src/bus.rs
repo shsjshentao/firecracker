f669416820f6efb0b59590e1cef92a8892e57bd8
Andrei Răduță <andrei.raduta11@gmail.com>
2020-05-30 13:34:00 +0300

    PCI: Improve the PciBus class
    - Add error signaling at adding a device or a bus.
    - Add add/get/remove methods for the connected busses and devices.
    - Add methods to read/write a register from a function of a device.
    - Add tests for the last two mentioned points.
    
    Signed-off-by: Andrei Răduță <andrei.raduta11@gmail.com>

--- src/pci/src/bus.rs
+++ src/pci/src/bus.rs
@@ -3,67 +3,202 @@
 
 use crate::constants::{MAX_BUS_NUMBER, MAX_DEVICE_NUMBER};
 use crate::device::PciDevice;
+use std::collections::HashMap;
+use std::option::Option;
+use std::sync::{Arc, Mutex};
 
-/// Errors for the Pci Bus.
+/// Errors for the PciBus.
 #[derive(Debug)]
 pub enum PciBusError {
-    /// Could not find an available device slot on the PCI bus.
-    NoPciDeviceSlotAvailable,
+    /// Invalid PCI bus number provided.
+    InvalidPciBusNumber(usize),
+    /// Valid PCI bus number  but already used.
+    AlreadyInUsePciBusSlot(usize),
+    /// Invalid PCI device number provided.
+    InvalidPciDeviceNumber(usize),
+    /// Valid PCI device number but already used.
+    AlreadyInUsePciDeviceSlot(usize),
 }
 
 pub type Result<T> = std::result::Result<T, PciBusError>;
 
 /// Each Bus must be assigned a unique bus number.
 /// The initial Bus Number, Bus 0, is typically assigned to the Root Complex.
-#[derive(Clone)]
 pub struct PciBus {
-    number: u32,
+    /// The number of the bus.
+    number: usize,
 
-    buses: Vec<PciBus>,
-    devices: Vec<PciDevice>,
+    /// The other buses that are connected to this bus.
+    buses: HashMap<usize, Arc<Mutex<PciBus>>>,
+
+    /// The device that are connected to this bus.
+    devices: HashMap<usize, Arc<Mutex<PciDevice>>>,
 }
 
 impl PciBus {
-    pub fn new(number: u32) -> Self {
+    pub fn new(number: usize) -> PciBus {
         PciBus {
             number,
-            buses: Vec::with_capacity(MAX_BUS_NUMBER),
-            devices: Vec::with_capacity(MAX_DEVICE_NUMBER),
+            buses: HashMap::new(),
+            devices: HashMap::new(),
         }
     }
 
-    /// Return the bus number.
-    pub fn get_number(&self) -> u32 {
+    /// Create a dummy PCI bus which contains a dummy PCI device on the 0 slot.
+    /// - `number` - the number of the bus.
+    pub fn new_dummy(number: usize) -> PciBus {
+        let mut bus = PciBus::new(number);
+
+        bus.add_device(PciDevice::new_dummy(0)).unwrap();
+
+        bus
+    }
+
+    /// Return the number of this device.
+    pub fn get_number(&self) -> usize {
         self.number
     }
 
-    /// Return a reference to the array of buses.
-    pub fn get_buses(&self) -> &Vec<PciBus> {
-        &self.buses
-    }
+    /// Add a new bus to the current bus.
+    /// * `bus` - The bus that will be wrapped in an Arc-Mutex struct and added.
+    pub fn add_bus(&mut self, bus: PciBus) -> Result<()> {
+        let bus_number = bus.get_number();
 
-    /// Return a mutable reference to the array of buses.
-    pub fn get_mut_buses(&mut self) -> &mut Vec<PciBus> {
-        &mut self.buses
-    }
-
-    /// Return a reference to the array of devices.
-    pub fn get_devices(&self) -> &Vec<PciDevice> {
-        &self.devices
-    }
-
-    /// Return a mutable reference to the array of devices.
-    pub fn get_mut_devices(&mut self) -> &mut Vec<PciDevice> {
-        &mut self.devices
-    }
-
-    pub fn add_device(&mut self, device: PciDevice) -> Result<()> {
-        if self.devices.len() == MAX_DEVICE_NUMBER {
-            return Err(PciBusError::NoPciDeviceSlotAvailable);
+        if bus_number >= MAX_BUS_NUMBER {
+            return Err(PciBusError::InvalidPciBusNumber(bus_number));
         }
 
-        self.devices.push(device);
+        if self.buses.contains_key(&bus_number) {
+            return Err(PciBusError::AlreadyInUsePciBusSlot(bus_number));
+        }
+
+        self.buses.insert(bus_number, Arc::new(Mutex::new(bus)));
         Ok(())
+    }
+
+    /// Return a reference to the requested bus if it exists.
+    /// * `bus` - The index of the bus connected on the current bus.
+    pub fn get_bus(&self, bus: usize) -> Option<&Arc<Mutex<PciBus>>> {
+        self.buses.get(&bus)
+    }
+
+    /// Return a mutable reference to the requested bus if it exists.
+    /// * `bus` - The index of the bus connected on the current bus.
+    pub fn get_mut_bus(&mut self, bus: usize) -> Option<&mut Arc<Mutex<PciBus>>> {
+        self.buses.get_mut(&bus)
+    }
+
+    /// Remove the bus from the current bus, returning the object, if it exists.
+    /// * `bus` - The index of the bus connected on the current bus.
+    pub fn remove_bus(&mut self, bus: usize) -> Option<Arc<Mutex<PciBus>>> {
+        self.buses.remove(&bus)
+    }
+
+    /// Add a new device to the current bus.
+    /// * `device` - The device that will be wrapped in an Arc-Mutex struct and added.
+    pub fn add_device(&mut self, device: PciDevice) -> Result<()> {
+        let device_number = device.get_number();
+
+        if device_number >= MAX_DEVICE_NUMBER {
+            return Err(PciBusError::InvalidPciDeviceNumber(device_number));
+        }
+
+        if self.devices.contains_key(&device_number) {
+            return Err(PciBusError::AlreadyInUsePciDeviceSlot(device_number));
+        }
+
+        self.devices
+            .insert(device_number, Arc::new(Mutex::new(device)));
+        Ok(())
+    }
+
+    /// Return a reference to the requested device if it exists.
+    /// * `device` - The index of the device connected on the current bus.
+    pub fn get_device(&self, device: usize) -> Option<&Arc<Mutex<PciDevice>>> {
+        self.devices.get(&device)
+    }
+
+    /// Return a mutable reference to the requested device if it exists.
+    /// * `device` - The index of the device connected on the current bus.
+    pub fn get_mut_device(&mut self, device: usize) -> Option<&mut Arc<Mutex<PciDevice>>> {
+        self.devices.get_mut(&device)
+    }
+
+    /// Remove the device from the current bus, returning the object, if it exists.
+    /// * `device` - The index of the device connected on the current bus.
+    pub fn remove_device(&mut self, device: usize) -> Option<Arc<Mutex<PciDevice>>> {
+        self.devices.remove(&device)
+    }
+
+    /// Get a register from the configuration header space of a function of the device.
+    /// Check if the device is on this bus or maybe on other busses connected.
+    /// * `bus` - The index of the bus.
+    /// * `device` - The index of the device of the bus.
+    /// * `function` - The index of the function of the device.
+    /// * `register` - The index of the register within configuration header space.
+    pub fn read_configuration_register(
+        &self,
+        bus: usize,
+        device: usize,
+        function: usize,
+        register: usize,
+    ) -> Option<u32> {
+        // Check if the message is for a device on this bus or check the other buses.
+        if bus == self.number {
+            return match self.get_device(device) {
+                Some(device) => device
+                    .lock()
+                    .unwrap()
+                    .read_configuration_register(function, register),
+                _ => None,
+            };
+        }
+
+        if let Some(bridge) = self.get_bus(bus) {
+            bridge
+                .lock()
+                .unwrap()
+                .read_configuration_register(bus, device, function, register)
+        } else {
+            None
+        }
+    }
+
+    /// Set a register in the configuration header space of a function of the device.
+    /// Check if the device is on this bus or maybe on other busses connected.
+    /// * `bus` - The index of the bus.
+    /// * `device` - The index of the device of the bus.
+    /// * `function` - The index of the function of the device.
+    /// * `register` - The index of the register within configuration header space.
+    /// * `offset` - The offset within the register.
+    /// * `data` - The actual bytes of data.
+    pub fn write_configuration_register(
+        &mut self,
+        bus: usize,
+        device: usize,
+        function: usize,
+        register: usize,
+        offset: usize,
+        data: &[u8],
+    ) {
+        // Check if the message is for a device on this bus or check the other buses.
+        if bus == self.number {
+            if let Some(device) = self.get_device(device) {
+                device
+                    .lock()
+                    .unwrap()
+                    .write_configuration_register(function, register, offset, data);
+            }
+
+            return;
+        }
+
+        if let Some(bridge) = self.get_bus(bus) {
+            bridge
+                .lock()
+                .unwrap()
+                .write_configuration_register(bus, device, function, register, offset, data);
+        }
     }
 }
 
@@ -71,18 +206,39 @@ impl PciBus {
 mod tests {
     use super::*;
 
-    /// Test the functions that works with the devices of the bus.
     #[test]
-    fn bus_devices() {
-        let mut bus = PciBus::new(0);
-        let device = PciDevice::empty();
+    fn bus_bus_add_get_remove() {
+        let mut main_bus = PciBus::new(0);
 
-        assert_eq!(bus.get_buses().len(), 0);
-        assert_eq!(bus.get_devices().len(), 0);
-
-        for _ in 0..MAX_DEVICE_NUMBER {
-            assert!(bus.add_device(device.clone()).is_ok());
+        for bus in 0..MAX_BUS_NUMBER {
+            assert!(main_bus.add_bus(PciBus::new_dummy(bus)).is_ok());
+            assert!(main_bus.get_bus(bus).is_some());
         }
-        assert!(bus.add_device(device.clone()).is_err());
+
+        assert!(main_bus.add_bus(PciBus::new_dummy(MAX_BUS_NUMBER)).is_err());
+
+        for bus in 0..MAX_BUS_NUMBER {
+            main_bus.remove_bus(bus);
+            assert!(main_bus.get_bus(bus).is_none());
+        }
+    }
+
+    #[test]
+    fn bus_device_add_get_remove() {
+        let mut bus = PciBus::new(0);
+
+        for device in 0..MAX_DEVICE_NUMBER {
+            assert!(bus.add_device(PciDevice::new_dummy(device)).is_ok());
+            assert!(bus.get_device(device).is_some());
+        }
+
+        assert!(bus
+            .add_device(PciDevice::new_dummy(MAX_DEVICE_NUMBER))
+            .is_err());
+
+        for device in 0..MAX_DEVICE_NUMBER {
+            bus.remove_device(device);
+            assert!(bus.get_device(device).is_none());
+        }
     }
 }
